90ff8d66b3da788a5b44844e0d4e56d8a9c8dcd2
mollika123 <aktermollika49@gmail.com>
2026-02-22 09:53:58 +0100

    added script file

---
+++ js/script.js
@@ -0,0 +1,3 @@
+const allFilterBtn=document.getElementById('all-filter-btn');
+const interviewFilterBtn=document.getElementById('interview-filter-btn');
+const rejectedFilterBtn=document.getElementById('rejected-filter-btn');
